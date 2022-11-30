9cd212b22e0068ba47aa7005cae99399c60bed8d
Lospel <rlaehdnl@gmail.com>
2022-11-30 16:06:05 +0900

    updated

--- app.py
+++ app.py
@@ -15,6 +15,10 @@ def main() :
     choice = st.selectbox("휴먼 프로그래밍 언어", proLan) 
     st.write(f"{choice} 언어를 선택함")
 
+    mulChoice = st.multiselect("언어 선택", proLan, default="SQL")
+    st.write("선택:", mulChoice)
+
+
 if __name__ == "__main__":
     main()
 
