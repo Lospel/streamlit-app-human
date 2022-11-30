e01decd8a0a43aa7a291cb0941b0cae4b220d529
Lospel <rlaehdnl@gmail.com>
2022-11-30 16:04:14 +0900

    updated

--- app.py
+++ app.py
@@ -8,27 +8,12 @@ import pandas as pd
 
 def main() :
     """코드작성"""
-    # 버튼 입력
-    name = 'Held'
+    # Select / Slider
 
-    if st.button("대문자"):
-        st.write(f'이름 : {name.upper()}')
-    if st.button("소문자"):
-        st.write(f'이름 : {name.lower()}')
-    
-    # 활성화, 비활성화
-    status = st.radio('Status', ("활성화", "비활성화"))
-    if status == "활성화":
-        st.success("활성화 상태")
-    else:
-        st.error("비활성화 상태")
-
-    # 체크박스
-    if st.checkbox("Show/Hide"):
-        st.text("무언가를 보여줘요")
-        
-    with st.expander('python'):
-        st.text("Hello Python")
+    # 프로그래밍 언어
+    proLan = ["파이썬", "자바", "R", "SQL"]
+    choice = st.selectbox("휴먼 프로그래밍 언어", proLan) 
+    st.write(f"{choice} 언어를 선택함")
 
 if __name__ == "__main__":
     main()
