39d982d6a9796bc826aec3fea958dfee9fe85cae
Lospel <rlaehdnl@gmail.com>
2022-11-30 15:25:59 +0900

    updated

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ def main() :
     """코드작성"""
     iris_df = pd.read_csv("data/iris.csv")
     st.dataframe(iris_df)
+    st.dataframe(iris_df, width=500, height=200)
 
 if __name__ == "__main__":
     main()
